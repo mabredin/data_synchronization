bd180ea11f5b3dbfe91adcc6ca968e6bba3ad5ab
mabredin <mbredin@bk.ru>
2023-10-24 12:02:45 +0300

    Add type hints in config.py

--- config.py
+++ config.py
@@ -1,15 +1,17 @@
 import os
+from typing import Optional
 
 from dotenv import load_dotenv
 
 
 load_dotenv()
 
-FILE_NAME = os.getenv('FILE_NAME')
+FILE_NAME: Optional[str] = os.getenv('FILE_NAME')
+COUNT_DAYS_FOR_SEARCH: Optional[str] = os.getenv('COUNT_DAYS_FOR_SEARCH')
 
 # Database
-DB_HOST = os.getenv('DB_HOST')
-DB_PORT = os.getenv('DB_PORT')
-DB_NAME = os.getenv('DB_NAME')
-DB_USER_NAME = os.getenv('DB_USER_NAME')
-DB_USER_PASSWORD = os.getenv('DB_USER_PASSWORD')
+DB_HOST: Optional[str] = os.getenv('DB_HOST')
+DB_PORT: Optional[str] = os.getenv('DB_PORT')
+DB_NAME: Optional[str] = os.getenv('DB_NAME')
+DB_USER_NAME: Optional[str] = os.getenv('DB_USER_NAME')
+DB_USER_PASSWORD: Optional[str] = os.getenv('DB_USER_PASSWORD')
